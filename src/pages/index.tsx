0fa2f9248e5bb0c810e0f3d4ca7c78f33ce437e1
takabayashi <you@example.com>
2023-02-14 18:24:09 +0900

    add-design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,11 +40,20 @@ const IndexPage: NextPage<IndexPageProps> = ({ initialCatImageUrl }) => {
 
     return (
         <div>
-            <button onClick={handleClick}>
+            <button 
+            onClick={handleClick}
+            style={{
+                backgroundColor: "#319795",
+                border: "none",
+                borderRadius: "4px",
+                color: "white",
+                padding: "4px 8px",
+              }}
+            >
                 きょうのにゃんこ🐱
             </button>
-            <div style={{ marginTop: 8 }}>
-                <img src={catImageUrl} width={500} height="auto" />
+            <div style={{ marginTop: 8, maxWidth: 500 }}>
+                <img src={catImageUrl} width={500} height="auto" alt="猫" />
             </div>
         </div>
     );
